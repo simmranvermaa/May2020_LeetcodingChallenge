7c2a7928a78d137850330ae5052a454be3bc1863
Simran Verma <36468747+simmranvermaa@users.noreply.github.com>
2020-05-30 00:16:04 +0530

    Create OnlineStockSpan.java

---
+++ OnlineStockSpan.java
@@ -0,0 +1,20 @@
+class StockSpanner {
+    Stack<Integer> prices, weight;
+    
+    public StockSpanner() {
+        prices=new Stack();
+        weight=new Stack();
+    }
+    
+    public int next(int price) {
+        int w=1;
+        while(!prices.isEmpty() && prices.peek()<=price){
+            prices.pop();
+            w+=weight.pop();
+        }
+        prices.push(price);
+        weight.push(w);
+        return w;
+    }
+}
+
